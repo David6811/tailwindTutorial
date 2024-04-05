c8216d5feb0d2eb3d52522ce283f5eaaa0842c97
weixu <xuwei19850423@gmail.com>
2024-04-05 18:23:23 +1100

    Columns

--- app/Columns/page.tsx
+++ app/Columns/page.tsx
@@ -1,7 +1,31 @@
+import Image from 'next/image';
+import demo from '../images/demo.png';
+
+
 export default function Page() {
     return (
-        <div className="w-full min-h-screen">
-           
+        <div>
+            {/* Adding based on column count */}
+            <div className="columns-3">
+                <Image src={demo} alt='' />
+                <Image src={demo} alt='' />
+                <Image src={demo} alt='' />
+            </div>
+
+            {/* Adding based on column width */}
+            <div className="columns-3xs mt-3">
+                <Image className="w-full aspect-video" src={demo} alt='' />
+                <Image className="w-full aspect-square" src={demo} alt='' />
+                <Image className="w-full aspect-video" src={demo} alt='' />
+            </div>
+
+            {/* Setting the column gap */}
+            <div className="gap-8 columns-3">
+                <Image className="w-full aspect-video" src={demo} alt='' />
+                <Image className="w-full aspect-video" src={demo} alt='' />
+                <Image className="w-full aspect-video" src={demo} alt='' />
+            </div>
+
         </div>
     );
 }
